f4a5e850ca38fa6cccf56eb6116741b83cbf82c3
krutobok <sasha020604336618@gmail.com>
2022-06-21 12:52:55 +0300

    save commit

---
+++ app/js/main.js
@@ -0,0 +1,4 @@
+const topSliderItems = document.querySelectorAll('.top-slider__item')
+
+topSliderItems.forEach(elem => elem.style.backgroundImage = elem.style.backgroundImage + ', linear-gradient(180deg, rgba(0, 0, 0, 0.2) -13.19%, rgba(0, 0, 0, 0.85) 94.01%)')
+
