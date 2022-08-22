f3d8fc5683e1ed62b7a3d4d8a3b1324e2fb2ee66
krutobok <sasha020604336618@gmail.com>
2022-08-22 18:18:41 +0300

    finish desktop

--- app/js/main.js
+++ app/js/main.js
@@ -212,9 +212,10 @@ const modalInner = document.querySelector('.modal__inner')
 const form = document.querySelector('.modal__form')
 const modalBg = document.querySelector('.modal__bg')
 const modalBtnClose = document.querySelector('.btn__modal--close')
-const modalBtnSubmit = document.querySelector('.modal__btn')
+// const modalBtnSubmit = document.querySelector('.modal__btn')
 const modalTitle = document.querySelector('.modal__title')
 const modalTextarea = document.querySelector('.modal__textarea')
+const modalCheckbox = document.querySelector('.modal__checkbox')
 const modalText = document.querySelector('.modal__text ')
 const input1 = document.querySelector('.modal__input--1')
 const input2 = document.querySelector('.modal__input--2')
@@ -235,6 +236,13 @@ function modal(){
                 modalTitle.textContent = 'Поговоримо?'
                 modalText.textContent = 'Заповніть форму, і ми звяжемося та обговоримо з вами усі деталі'
                 modalTextarea.classList.add('active')
+                modalTextarea.setAttribute('placeholder', 'Тема')
+            }
+            else if (btn.getAttribute('data-modal-num') === '3'){
+                modalTitle.textContent = 'Хочете стати нашим партнером?'
+                modalText.textContent = 'Заповніть форму, і ми звяжемося та обговоримо з вами усі деталі'
+                modalTextarea.classList.add('active')
+                modalTextarea.setAttribute('placeholder', 'Коротко про себе')
             }
         })
     })
@@ -254,22 +262,26 @@ function modal(){
     })
     input1.addEventListener('input', ()=> input1.classList.remove('invalid'))
     input2.addEventListener('input', ()=> input2.classList.remove('invalid'))
+    modalCheckbox.addEventListener('change', ()=>{modalCheckbox.closest('label').classList.remove('invalid')})
     function onSubmit(event) {
         event.preventDefault();
+        let counter = 3
         const inputValue1 = input1.value
         const inputValue2 = input2.value
-        if (inputValue1 !== '' && inputValue2 !== ''){
+        if (inputValue2 === ''){
+            input2.classList.add('invalid')
+            counter--
+        }
+        if (inputValue1 === ''){
+            input1.classList.add('invalid')
+            counter--
+        }
+        if (!modalCheckbox.checked){
+            modalCheckbox.closest('label').classList.add('invalid')
+            counter--
+        }
+        if (counter >= 3){
             thank()
-        }
-        else if (inputValue1 !== '' && inputValue2 === ''){
-            input2.classList.add('invalid')
-        }
-        else if (inputValue1 === '' && inputValue2 !== ''){
-            input1.classList.add('invalid')
-        }
-        else if (inputValue1 === '' && inputValue2 === ''){
-            input1.classList.add('invalid')
-            input2.classList.add('invalid')
         }
     }
     function thank() {
@@ -415,6 +427,67 @@ function getCoords(elem) {
         left: box.left + window.pageXOffset
     };
 }
+
+const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
+    animationTime = 500,
+    framesCount = 120;
+
+anchors.forEach(function(item) {
+    item.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
+        let coordY2 = (coordY-window.pageYOffset)
+        console.log('coordY2: ' + coordY2)
+        let scroller = setInterval(function() {
+            let scrollBy = (Math.abs(coordY2) / framesCount);
+            if (coordY2>0){
+                if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+                    window.scrollBy(0, scrollBy);
+                } else {
+                    window.scrollTo(0, coordY);
+                    clearInterval(scroller);
+                }
+            }
+            else if (coordY2<0){
+                if(scrollBy < window.pageYOffset - coordY) {
+                    window.scrollBy(0, -scrollBy);
+                } else {
+                    window.scrollTo(0, coordY);
+                    clearInterval(scroller);
+                }
+            }
+            else if (coordY2){
+                clearInterval(scroller);
+            }
+        }, animationTime / framesCount);
+    });
+});
+
+
+const scrollItems = document.querySelectorAll('.animate');
+
+const scrollAnimation = () => {
+    let windowCenter = window.innerHeight + window.scrollY;
+    scrollItems.forEach(el => {
+        let scrollOffset = getCoords(el).top + (el.offsetHeight / 2);
+        console.log(scrollOffset)
+        console.log(windowCenter-el.offsetHeight)
+        if (windowCenter-el.offsetHeight >= scrollOffset) {
+            if (windowCenter-window.innerHeight < scrollOffset){
+                el.classList.add('animation-active');
+            }
+        }
+    });
+};
+
+
+scrollAnimation();
+window.addEventListener('scroll', () => {
+    scrollAnimation();
+});
+
+
 
 
 slider('.top-slider__content')
